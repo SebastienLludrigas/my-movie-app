d28bf36f6899b84ef763a7dcc8f7409314d1e213
SebastienLludrigas <sebastienlludrigas@gmail.com>
2023-07-29 09:07:14 +0200

    part1 done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import styles from "./Header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <p>
+          <Link href="/">MyMovieApp</Link>
+        </p>
+      </div>
+      <div className={styles.navigation}>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/series">Séries</Link>
+            </li>
+            <li>
+              <Link href="/movies">Films</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <input type="text" placeholder="Rechercher un titre..." />
+      <div>
+        <FontAwesomeIcon icon={faUser} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/media-card/MediaCard.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import styles from "./MediaCard.module.scss";
+import Image from "next/image";
+
+const MediaCard = ({ mediaId }) => {
+  return (
+    <div className={styles.card}>
+      <Link href={`/movies/${mediaId}`}>
+        <div className={styles.image}>
+          <Image
+            src="https://image.tmdb.org/t/p/w500/hYQs5RPHiWctoYqvI8baHiIqdd8.jpg"
+            alt="media title"
+            fill
+          />
+        </div>
+        <div className={styles.content}>
+          <h2>Creed III</h2>
+          <p>Le 01/03/2023</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default MediaCard;
